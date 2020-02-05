2f26380b5542c52e9405342c8c1fd704f4ca7166
martinberlin <martin@luckycloud.de>
2020-02-05 22:57:16 +0100

    Fix carrousel

--- public/js/engine/common.js
+++ public/js/engine/common.js
@@ -1,4 +1,3 @@
-//demo myCarousel demonstrating the slid and slide events
 document.addEventListener("DOMContentLoaded", function(event) {
     var mainSlider = document.getElementById('myCarousel');
     var mainSliderItems = mainSlider.querySelectorAll('.item');
@@ -8,12 +7,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var activeCaption = mainSliderItems[currentActive].querySelector('.carousel-caption');
         activeCaption.classList.remove('slide');
     });
-
     mainSlider.addEventListener('slid.bs.carousel', function(e) {
         var activeCaption = e.relatedTarget.querySelector('.carousel-caption');
         activeCaption.classList.add('slide');
     });
-
-    var caleCarousel = new Carousel(mainSlider);
 });
 
