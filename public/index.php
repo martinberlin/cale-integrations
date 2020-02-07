2dd891ee573eac52a80e2c58ea13faf05c926522
Martin Fasani <fasani@skygate.de>
2020-02-07 15:17:56 +0100

    Downgrade to Symfony 4.4 since PHP version is not high enough in AWS EC2

--- public/index.php
+++ public/index.php
@@ -1,13 +1,10 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/vendor/autoload.php';
-
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+require dirname(__DIR__).'/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
