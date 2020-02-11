0a9ffeb1b0d96ba0f7f4dd3f3658307a28ec2007
Martin Fasani <fasani@skygate.de>
2020-02-11 17:50:05 +0100

    #2 Location fix. Needs refactoring

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -13,6 +13,7 @@ use App\Repository\UserRepository;
 use App\Service\GoogleClientService;
 use Doctrine\ORM\EntityManagerInterface;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -102,136 +103,12 @@ class BackendController extends AbstractController
     }
 
     /**
-     * @Route("/api/google/calendar/{uuid}/{intapi_uuid}/{step}", name="b_api_wizard_cale-google")
-     */
-    public function apiWizardGoogleCalendar(
-        $uuid, $intapi_uuid = "", $step = 1, Request $request, UserApiRepository $userApiRepository,
-        IntegrationApiRepository $intApiRepository, EntityManagerInterface $entityManager, \Google_Client $googleClient)
-    {
-        // DEBUG ONLY - Add your test proxy
-        if ($request->getClientIp() === '127.0.0.1' && (isset($_ENV['API_PROXY']))) {
-            $httpClient = new Client([
-                'proxy' => $_ENV['API_PROXY'],
-                'verify' => false
-            ]);
-            $googleClient->setHttpClient($httpClient);
-        }
-
-        $userApi = $userApiRepository->findOneBy(['uuid' => $uuid]);
-        if (!$userApi instanceof UserApi) {
-            throw $this->createNotFoundException("$uuid is not a valid API definition");
-        }
-        if ($userApi->getUser() !== $this->getUser()) {
-            throw $this->createNotFoundException("You don't have access to API $uuid");
-        }
-        $api = new IntegrationApi();
-        if ($intapi_uuid !== "") {
-            $api = $intApiRepository->findOneBy(['uuid' => $intapi_uuid]);
-        }
-        if (!$api instanceof IntegrationApi) {
-            throw $this->createNotFoundException("$intapi_uuid is not a valid integration API");
-        }
-        $authUrl = '';
-        switch ($step) {
-            case 2:
-                $title = "Step 2: Accept read-only access and copy the generated Token";
-                $form = $this->createForm(ApiTokenType::class, $userApi);
-                $googleClient->setScopes(\Google_Service_Calendar::CALENDAR_READONLY);
-                $googleClient->setAccessType('offline'); // offline
-                $googleClient->setPrompt('select_account consent');
-                $credentials = json_decode($userApi->getCredentials(),true);
-                $key = isset($credentials['installed']) ? 'installed' : 'web';
-
-                foreach ($credentials[$key] as $ck=>$cv) {
-                    $googleClient->setConfig($ck,$cv);
-                    switch ($ck) {
-                        case 'client_id':
-                            $googleClient->setClientId($cv);
-                            break;
-                        case 'client_secret':
-                            $googleClient->setClientSecret($cv);
-                            break;
-                        case 'redirect_uris':
-                            $googleClient->setRedirectUri($cv[0]);
-                            break;
-                    }
-                }
-                // Request authorization from the user.
-                $authUrl = $googleClient->createAuthUrl();
-
-                break;
-            default:
-                $title = 'Step 1: Turn on the Google Calendar API';
-                $form = $this->createForm(GoogleCalendar1Type::class, $api);
-        }
-
-        $form->handleRequest($request);
-        $error = "";
-        $apiUuid = "";
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            switch ($step) {
-                case 1:
-                $credentialsFileUpload = $form->get('credentialsFile')->getData();
-                if ($credentialsFileUpload) {
-                    $userApi->setCredentials(file_get_contents($credentialsFileUpload->getPathname()));
-                } else {
-                    $this->addFlash('error', "Error reading credentials file");
-                }
-
-                $api->setUserApi($userApi);
-                try {
-                    $entityManager->persist($api);
-
-                } catch (\Exception $e) {
-                    $error = $e->getMessage();
-                    $this->addFlash('error', $error);
-                }
-               break;
-                case 2:
-                    // todo Read: https://developers.google.com/calendar/quickstart/php
-                    // Exchange authorization code for an access token.
-                    //dump($userApi->getAccessToken());exit();
-                    $accessToken = $googleClient->fetchAccessTokenWithAuthCode($userApi->getAccessToken());
-                    $googleClient->setAccessToken($accessToken);
-                    $userApi->setJsonToken(json_encode($googleClient->getAccessToken()));
-                    break;
-            }
-
-            if ($error === '') {
-                $entityManager->persist($userApi);
-                $entityManager->flush();
-                $this->addFlash('success', 'Credentials saved');
-                $apiUuid = $api->getId();
-                return $this->redirectToRoute('b_api_wizard_cale-google',
-                    [
-                        'uuid' => $uuid,
-                        'intapi_uuid' => $apiUuid,
-                        'step' => 2
-                    ]);
-            }
-        }
-
-        return $this->render(
-            "backend/api/wizard/google/cale-google-{$step}.html.twig",
-            [
-                'title' => $title,
-                'form'  => $form->createView(),
-                'api_uuid' => $apiUuid,
-                'step'  => $step,
-                'authUrl' => $authUrl
-            ]
-        );
-
-    }
-
-    /**
      * TODO: This uuid is of the User API but on saving should set also the uuid of IntApi
      *       since with same api token you could query different cities
-     * @Route("/api/customize/location/{uuid}", name="b_api_customize_location")
+     * @Route("/api/customize/location/{uuid}/{intapi_uuid}", name="b_api_customize_location")
      */
     public function apiCustomizeLocation(
-        $uuid, Request $request,UserApiRepository $userApiRepository,
+        $uuid, $intapi_uuid = "", Request $request,UserApiRepository $userApiRepository,
         IntegrationApiRepository $intApiRepository, EntityManagerInterface $entityManager)
     {
         $step = 1; // Default wizard step
@@ -243,10 +120,12 @@ class BackendController extends AbstractController
         if ($userApi->getUser() !== $this->getUser()){
             throw $this->createNotFoundException("You don't have access to API $uuid");
         }
-
-        $api = $intApiRepository->findOneBy(['userApi'=>$userApi]);
-        if ($api instanceof IntegrationApi === false) {
-            $api = new IntegrationApi();
+        $api = new IntegrationApi();
+        if ($intapi_uuid !== "") {
+            $api = $intApiRepository->findOneBy(['uuid' => $intapi_uuid]);
+        }
+        if (!$api instanceof IntegrationApi) {
+            throw $this->createNotFoundException("$intapi_uuid is not a valid integration API");
         }
         if ($this->getUser()->getLanguage()!=="" && $api->getLanguage() === "") {
           $api->setLanguage($this->getUser()->getLanguage());
@@ -278,6 +157,11 @@ class BackendController extends AbstractController
                 $entityManager->flush();
                 $this->addFlash('success', 'Location API configuration saved');
                 $apiUuid = $api->getId();
+                return $this->redirectToRoute('b_api_customize_location',
+                    [
+                        'uuid' => $uuid,
+                        'intapi_uuid' => $apiUuid,
+                    ]);
             }
         }
 
@@ -286,7 +170,7 @@ class BackendController extends AbstractController
             [
                 'title' => 'Step 1: Api customize location Api',
                 'form'  => $form->createView(),
-                'api_uuid' => $apiUuid,
+                'intapi_uuid' => $intapi_uuid,
                 'step'  => $step
             ]
         );
@@ -347,17 +231,17 @@ class BackendController extends AbstractController
     }
 
     /**
-     * @Route("/api/rest/{uuid}", name="b_api_request")
+     * @Route("/api/json/{int_api_id}", name="b_api_request")
      */
-    public function apiJsonPreview(Request $request, $uuid = null, IntegrationApiRepository $intApiRepository)
+    public function apiJsonPreview(Request $request, $int_api_id = null, IntegrationApiRepository $intApiRepository)
     {
         $options = [];
         if (isset($_ENV['API_PROXY'])) {
             $options = array('proxy' => 'http://'.$_ENV['API_PROXY']);
         }
-        $intApi = $intApiRepository->findOneBy(['uuid'=>$uuid]);
+        $intApi = $intApiRepository->findOneBy(['uuid'=>$int_api_id]);
         if ($intApi instanceof IntegrationApi === false) {
-           return $this->createNotFoundException("Integrated API not found with ID $uuid");
+           return $this->createNotFoundException("Integrated API not found with ID $int_api_id");
         }
 
         // https://api.darksky.net/forecast/[token]/[latitude],[longitude]
@@ -392,13 +276,14 @@ class BackendController extends AbstractController
     }
 
     /**
-     * @Route("/api/cale-google/{uuid}", name="b_api_cale-google")
+     * Wizard to configure Google Oauth. Example: https://developers.google.com/calendar/quickstart/php
+     * @Route("/api/google/calendar/{uuid}/{intapi_uuid}/{step}", name="b_api_wizard_cale-google")
      */
-    public function apiGoogleServiceCalendarJson(
-        $uuid = null, Request $request,
-        IntegrationApiRepository $intApiRepository)
+    public function apiWizardGoogleCalendar(
+        $uuid, $intapi_uuid = "", $step = 1, Request $request, UserApiRepository $userApiRepository,
+        IntegrationApiRepository $intApiRepository, EntityManagerInterface $entityManager, \Google_Client $googleClient)
     {
-        $googleClient = new \Google_Client();
+        // DEBUG ONLY - Add your test proxy
         if ($request->getClientIp() === '127.0.0.1' && (isset($_ENV['API_PROXY']))) {
             $httpClient = new Client([
                 'proxy' => $_ENV['API_PROXY'],
@@ -406,9 +291,137 @@ class BackendController extends AbstractController
             ]);
             $googleClient->setHttpClient($httpClient);
         }
-        $intApi = $intApiRepository->findOneBy(['uuid'=>$uuid]);
+        $renderPreview = 0;
+        $userApi = $userApiRepository->findOneBy(['uuid' => $uuid]);
+        if (!$userApi instanceof UserApi) {
+            throw $this->createNotFoundException("$uuid is not a valid API definition");
+        }
+        if ($userApi->getUser() !== $this->getUser()) {
+            throw $this->createNotFoundException("You don't have access to API $uuid");
+        }
+        $api = new IntegrationApi();
+        if ($intapi_uuid !== "") {
+            $api = $intApiRepository->findOneBy(['uuid' => $intapi_uuid]);
+        }
+        if (!$api instanceof IntegrationApi) {
+            throw $this->createNotFoundException("$intapi_uuid is not a valid integration API");
+        }
+        $authUrl = '';
+        switch ($step) {
+            case 2:
+                if ($userApi->getJsonToken()!=="") {
+                    $renderPreview = 1;
+                }
+                $title = "Step 2: Accept read-only access and copy the generated Token";
+                $form = $this->createForm(ApiTokenType::class, $userApi);
+                $googleClient->setScopes(\Google_Service_Calendar::CALENDAR_READONLY);
+                $googleClient->setAccessType('offline'); // offline
+                $googleClient->setPrompt('select_account consent');
+                $credentials = json_decode($userApi->getCredentials(),true);
+                $key = isset($credentials['installed']) ? 'installed' : 'web';
+
+                foreach ($credentials[$key] as $ck=>$cv) {
+                    $googleClient->setConfig($ck,$cv);
+                    switch ($ck) {
+                        case 'client_id':
+                            $googleClient->setClientId($cv);
+                            break;
+                        case 'client_secret':
+                            $googleClient->setClientSecret($cv);
+                            break;
+                        case 'redirect_uris':
+                            $googleClient->setRedirectUri($cv[0]);
+                            break;
+                    }
+                }
+                // Request authorization from the user.
+                $authUrl = $googleClient->createAuthUrl();
+
+                break;
+            default:
+                $title = 'Step 1: Turn on the Google Calendar API';
+                $form = $this->createForm(GoogleCalendar1Type::class, $api);
+        }
+
+        $form->handleRequest($request);
+        $error = "";
+        $apiUuid = "";
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            switch ($step) {
+                case 1:
+                    $credentialsFileUpload = $form->get('credentialsFile')->getData();
+                    if ($credentialsFileUpload) {
+                        $userApi->setCredentials(file_get_contents($credentialsFileUpload->getPathname()));
+                    } else {
+                        $this->addFlash('error', "Error reading credentials file");
+                    }
+
+                    $api->setUserApi($userApi);
+                    try {
+                        $entityManager->persist($api);
+
+                    } catch (\Exception $e) {
+                        $error = $e->getMessage();
+                        $this->addFlash('error', $error);
+                    }
+                    break;
+                case 2:
+                    // Exchange authorization code for an access token.
+                    try {
+                        $accessToken = $googleClient->fetchAccessTokenWithAuthCode($userApi->getAccessToken());
+                        $googleClient->setAccessToken($accessToken);
+                        $userApi->setJsonToken(json_encode($googleClient->getAccessToken()));
+                    } catch (ClientException $ce) {
+                        $this->addFlash('success', $ce->getMessage());
+                    }
+                    break;
+            }
+
+            if ($error === '') {
+                $entityManager->persist($userApi);
+                $entityManager->flush();
+                $this->addFlash('success', 'Credentials saved');
+                $apiUuid = $api->getId();
+                return $this->redirectToRoute('b_api_wizard_cale-google',
+                    [
+                        'uuid' => $uuid,
+                        'intapi_uuid' => $apiUuid,
+                        'step' => 2
+                    ]);
+            }
+        }
+        return $this->render(
+            "backend/api/wizard/google/cale-google-{$step}.html.twig",
+            [
+                'title' => $title,
+                'form'  => $form->createView(),
+                'step'  => $step,
+                'authUrl' => $authUrl,
+                'api_uuid' => $apiUuid,
+                'intapi_uuid' => $intapi_uuid,
+                'renderPreview' => $renderPreview
+            ]
+        );
+    }
+
+    /**
+     * @Route("/api/cale-google/json/{int_api_id}", name="b_api_cale-google")
+     */
+    public function apiGoogleServiceCalendarJson(
+        $int_api_id = null, Request $request, \Google_Client $googleClient,
+        IntegrationApiRepository $intApiRepository)
+    {
+        if ($request->getClientIp() === '127.0.0.1' && (isset($_ENV['API_PROXY']))) {
+            $httpClient = new Client([
+                'proxy' => $_ENV['API_PROXY'],
+                'verify' => false
+            ]);
+            $googleClient->setHttpClient($httpClient);
+        }
+        $intApi = $intApiRepository->findOneBy(['uuid'=>$int_api_id]);
         if ($intApi instanceof IntegrationApi === false) {
-            return $this->createNotFoundException("Integrated API not found with ID $uuid");
+            return $this->createNotFoundException("Integrated API not found with ID $int_api_id");
         }
 
         $userApi = $intApi->getUserApi();
