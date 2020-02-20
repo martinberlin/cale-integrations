f9a0b14ef28911c1e9bc1bb1ec334178d92e30ae
Martin Fasani <fasani@skygate.de>
2020-02-20 17:47:33 +0100

    ES new language preparation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,41 +4,39 @@ namespace App\Controller;
 use App\Repository\ApiRepository;
 use App\Repository\DisplayRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
- * @Route("/")
+ * Class HomeController
+ * @package App\Controller
+ * This Routes are not working with annotations so are defined in routes.yaml
  */
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/", name="home")
-     */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->redirectToRoute('register', [], 301);
+
+        return $this->render(
+            $request->getLocale().'/www-index.html.twig'
+        );
+        //return $this->redirectToRoute('register', [], 301);
     }
 
-    /**
-     * @Route("/about-cale", name="about-cale")
-     */
-    public function aboutCale()
+    public function aboutCale(Request $request)
     {
         return $this->render(
-            'www-about-cale.html.twig'
+            $request->getLocale().'/www-about-cale.html.twig'
         );
     }
 
-    /**
-     * @Route("/eink-displays", name="displays")
-     */
-    public function displays(DisplayRepository $displayRepository)
+    public function displays(Request $request, DisplayRepository $displayRepository)
     {
         $displays = $displayRepository->findAll();
         return $this->render(
-            'www-display.html.twig',
+            $request->getLocale().'/www-display.html.twig',
         [
             'displays' => $displays
         ]
@@ -48,44 +46,41 @@ class HomeController extends AbstractController
     /**
      * @Route("/apis", name="apis")
      */
-    public function apis(ApiRepository $apiRepository)
+    public function apis(Request $request, ApiRepository $apiRepository)
     {
         $apis = $apiRepository->findAll();
         return $this->render(
-            'www-apis.html.twig',
+            $request->getLocale().'/www-apis.html.twig',
             [
                 'apis' => $apis
             ]
         );
     }
 
-    /**
-     * @Route("/built-transparently", name="transparently")
-     */
-    public function transparently()
+    public function transparently(Request $request)
     {
         return $this->render(
-            'www-built-transparently.html.twig'
+            $request->getLocale().'/www-built-transparently.html.twig'
         );
     }
 
     /**
      * @Route("/thanks", name="thanks")
      */
-    public function thanks()
+    public function thanks(Request $request)
     {
         return $this->render(
-            'thanks.html.twig'
+            $request->getLocale().'/thanks.html.twig'
         );
     }
 
     /**
      * @Route("/privacy-policy", name="privacy-policy")
      */
-    public function privacyPolicy()
+    public function privacyPolicy(Request $request)
     {
         return $this->render(
-            'www-privacy-policy.html.twig'
+            $request->getLocale().'/www-privacy-policy.html.twig'
         );
     }
 }
